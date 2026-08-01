927603be349b7f9fd76f0478a7bc2e052b9bbcdf
Tymon Tumialis <tymontumialis@gmail.com>
2026-08-01 19:03:04 +0200

    3 stage pipe is working

--- shell/utils.c
+++ shell/utils.c
@@ -154,7 +154,7 @@ static void run_pipe(pipline_struct* pipline, int current_cmd) {
   if (current_cmd >= pipline->n_cmds)
     return;
   int old_pipe_read = -1;
-  if (current_cmd > 0) {
+  if (current_cmd != 0 && current_cmd != pipline->n_cmds - 1) {
     old_pipe_read = pipe_fds[0];
     init_new_pipe();
   }
